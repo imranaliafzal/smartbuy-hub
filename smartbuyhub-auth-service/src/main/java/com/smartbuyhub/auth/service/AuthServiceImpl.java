d3add2fab527bb03a0edef4b3c45326e0c9470b5
imranafzal <imran.afzal@toshibagcs.com>
2025-06-07 09:36:15 -0400

    added auth related services and security configurations.

---
+++ smartbuyhub-auth-service/src/main/java/com/smartbuyhub/auth/service/AuthServiceImpl.java
@@ -0,0 +1,104 @@
+package com.smartbuyhub.auth.service;
+
+import com.smartbuyhub.auth.api.AuthenticationApi;
+import com.smartbuyhub.auth.api.model.JwtResponse;
+import com.smartbuyhub.auth.api.model.LoginRequest;
+import com.smartbuyhub.auth.api.model.SignupRequest;
+import com.smartbuyhub.auth.service.security.JwtUtils;
+import com.smartbuyhub.auth.service.security.UserDetailsImpl;
+import com.smartbuyhub.data.repository.RoleRepository;
+import com.smartbuyhub.data.repository.UserRepository;
+import com.smartbuyhub.domain.auth.Role;
+import com.smartbuyhub.domain.auth.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@RestController
+public class AuthServiceImpl implements AuthenticationApi {
+
+    @Autowired
+    AuthenticationManager authenticationManager;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    PasswordEncoder encoder;
+
+    @Autowired
+    JwtUtils jwtUtils;
+
+    @Override
+    public ResponseEntity<JwtResponse> authenticateUser(LoginRequest loginRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(item -> item.getAuthority())
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername(), userDetails.getEmail(), roles));
+    }
+
+    @Override
+    public ResponseEntity<?> registerUser(SignupRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Error: Username is already taken!");
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body("Error: Email is already in use!");
+        }
+
+        User user = new User(signUpRequest.getUsername(),
+                signUpRequest.getEmail(),
+                encoder.encode(signUpRequest.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        
+        if (signUpRequest.getRoles() == null || signUpRequest.getRoles().isEmpty()) {
+            Role userRole = roleRepository.findByName(Role.ERole.ROLE_USER)
+                    .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+        } else {
+            signUpRequest.getRoles().forEach(role -> {
+                if ("admin".equals(role)) {
+                    Role adminRole = roleRepository.findByName(Role.ERole.ROLE_ADMIN)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(adminRole);
+                } else {
+                    Role userRole = roleRepository.findByName(Role.ERole.ROLE_USER)
+                            .orElseThrow(() -> new RuntimeException("Error: Role is not found."));
+                    roles.add(userRole);
+                }
+            });
+        }
+
+        user.setRoles(roles);
+        userRepository.save(user);
+
+        return ResponseEntity.ok("User registered successfully!");
+    }
+}
