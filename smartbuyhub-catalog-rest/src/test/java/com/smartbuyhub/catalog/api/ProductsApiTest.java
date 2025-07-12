238c73461607b0748f0663c593396a39e58a6cf8
imranafzal <imran.afzal@toshibagcs.com>
2025-07-12 11:39:30 -0400

    Added test

---
+++ smartbuyhub-catalog-rest/src/test/java/com/smartbuyhub/catalog/api/ProductsApiTest.java
@@ -0,0 +1,38 @@
+package com.smartbuyhub.catalog.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductsApiTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deactivateProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void deleteProduct() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getAllProducts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getProductById() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void importProducts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void updateProduct() {
+    }
+}
---
+++ smartbuyhub-catalog-rest/src/test/java/com/smartbuyhub/catalog/controller/CatalogControllerTest.java
@@ -0,0 +1,70 @@
+package com.smartbuyhub.catalog.controller;
+
+import com.smartbuyhub.catalog.model.ProductDTO;
+import com.smartbuyhub.catalog.rest.CatalogServiceApplication;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest(classes = CatalogServiceApplication.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class CatalogControllerTest {
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getAllProducts() {
+        ResponseEntity<List<ProductDTO>> response = restTemplate.exchange(
+                "/api/products",
+                HttpMethod.GET,
+                null,
+                new ParameterizedTypeReference<>() {
+                }
+        );
+
+        assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
+
+    }
+
+    @Test
+    void getProductById() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void importProducts() {
+    }
+
+    @Test
+    void deleteProduct() {
+    }
+
+    @Test
+    void deactivateProduct() {
+    }
+
+    @Test
+    void addProduct() {
+    }
+}
